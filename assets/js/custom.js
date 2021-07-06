5ca7a42967732c8ccb1cff1830942f082293aedb
David Feltendal <davidfeltendal@gmail.com>
2021-07-06 12:13:50 +0200

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,8 @@
+function openSlideMenu(){
+    document.getElementById('menu').style.width = '380px';
+    document.getElementById('content').style.marginLeft = '380px';
+}
+function closeSlideMenu(){
+    document.getElementById('menu').style.width = '0px';
+    document.getElementById('content').style.marginLeft = '0px';
+}
